026ec9ee99d60d17f07b71bd106c5020129e4b0d
CHIBUIKE ANERO <Anero.chibykefilipe@gmail.com>
2026-04-07 12:26:04 +0100

    first commit

---
+++ newweb.js
@@ -0,0 +1,10 @@
+function toggleMenu(){
+    document.getElementById("navlinks").classList.toggle("show")
+}
+
+function validateForm(){
+    let name = document.getElementById("name").value;
+    let email = document.getElementById("email").value;
+    let password = document.getElementById("password").value;
+    let gender = document.getElementById("gender")
+}
